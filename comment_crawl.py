fd826d22450506e0d332a8f598da95413c9c9bf2
Jasonjiang27 <jiangyz1991@163.com>
2018-06-17 20:00:30 +0800

    update codes

--- comment_crawl.py
+++ comment_crawl.py
@@ -12,19 +12,19 @@ results = []
 def parse(response):
     for comment in response.css('div.CommentItem'):
         username = comment.xpath('.//a[@class="UserLink-link"]/text()').extract_first()
-        contents = comment.xpath('.//div[@class="RichText ztext CommentItem-content"]/p/text()').extract_first()
+        contents = comment.xpath('.//div[@class="RichText ztext CommentItem-content"]/text()').extract_first()
         time = comment.xpath('.//span[@class="CommentItem-time"]/text()').extract_first()
         result = {"username":username,"contents":contents,"time":time}
         results.append(result)
 
+
 def has_next_page(response):
-    next_page_botton=driver.find_element_by_link_text('下一页')
-    while next_page_botton:
+    page_ex=response.xpath('//botton[contains(@class,"Button PaginationButton PaginationButton-next Button--plain")]/@class').extract_first()
+    while page_ex:
         return True
 
-
 def go_to_next_page(driver):
-    next_page_botton=driver.find_element_by_link_text('下一页')
+    next_page_botton=driver.find_element_by_css_selector('button.PaginationButton-next')
     next_page_botton.click()
 
 def wait_page_return(driver,page):
@@ -46,13 +46,11 @@ def spider():
         html = driver.page_source
         response = HtmlResponse(url=url,body=html.encode('utf8'))
         parse(response)
-        next_page_botton=driver.find_element_by_link_text('下一页')
-       
         if not has_next_page(response):
             break
         page+=1
         go_to_next_page(driver)
-    with open('/home/shiyanlou/comments.json','w') as f:
+    with open('/home/shiyanlou/comments.json','w',encoding='utf-8') as f:
         f.write(json.dumps(results))
 
 if __name__=='__main__':
