991a26380b2ae8ca6441af40bbe7148b6c0d05aa
Jason Jiang <37802356+Jasonjiang27@users.noreply.github.com>
2018-06-21 22:52:44 +0800

    增加关于异步的注释部分

--- async-crawl/spider.py
+++ async-crawl/spider.py
@@ -1,18 +1,18 @@
 import csv
-import asyncio 
-import aiohttp
-import async_timeout
+import asyncio   #asyncio实现异步爬虫
+import aiohttp    #aiohttp实现网咯请求
+import async_timeout  #设置异步爬虫超时
 
-from scrapy.http import HtmlResponse
+from scrapy.http import HtmlResponse   #解析页面
 
-results = []
+results = []    
 
-
+#定义获取页面的异步操作
 async def fetch(session,url):
     with async_timeout.timeout(10):
         async with session.get(url) as response:
             return await response.text()
-
+#定义提取函数
 def parse(url,body):
     response = HtmlResponse(url=url,body=body)
     for repository in response.css('li.public'):
@@ -20,7 +20,7 @@ def parse(url,body):
         update_time = repository.xpath('.//relative-time/@datetime').extract_first()
         results.append((name,update_time))
 
-
+#定义异步执行任务
 async def task(url):
     async with aiohttp.ClientSession() as session:
         html = await fetch(session,url)
