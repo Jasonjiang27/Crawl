56ac5aca494909d3ed5d53119f43e9bc8bc0b2c8
Jasonjiang27 <jiangyz1991@163.com>
2018-06-29 12:54:26 +0800

    高德API＋python解决租房问题

---
+++ rent_project/crawl.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = 'http://bj.58.com/pinpaigongyu/pn/{page}/?minprice=2000_4000'
+
+page = 0
+
+csv_file = open("rent.csv", 'w')
+csv_writer = csv.writer(csv_file,delimiter=',')
+
+while True:
+    page+=1
+    print("fetch:",url.format(page=page))
+    response = requests.get(url.format(page=page))
+    html = BeautifulSoup(response.text,'lxml')
+    house_list = html.select(".list > li")
+
+    if not house_list:
+        break
+
+    for house in house_list:
+        house_title = house.select("h2")[0].string
+        house_url = "http://bj.58.com/%s"%(house.select("a")[0]["href"])
+        house_info_list = house_title.split()
+
+        if "公寓" in house_info_list[1] or "青年社区" in house_info_list[1]:
+            house_location = house_info_list[0]
+        else:
+            house_location = house_info_list[1]
+
+        house_money = house.select(".money")[0].select("b")[0].string
+        csv_writer.writerow([house_title, house_location, house_money, house_url])
+csv_file.close()
+
